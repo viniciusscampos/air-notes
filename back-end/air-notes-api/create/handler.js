3d5a5360c572139fe348ff858e9c6af30d852ec8
Vinicius Campos <silvcamposvinicius@gmail.com>
2018-06-06 00:44:39 -0300

    Add create note functionality.

---
+++ back-end/air-notes-api/create/handler.js
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk")
+
+module.exports.create = (event, context, callback) => {
+
+  const dynamoDb = new AWS.DynamoDB.DocumentClient()    
+  const request = event
+  const body = JSON.parse(request.body) 
+
+  var params = {
+    TableName: process.env.DYNAMODB_TABLE,  
+    Item: {
+        note: body.note
+    }
+  }
+
+  dynamoDb.put(params, (error) => {
+    if(error) {
+      console.log(error)
+
+      callback(null, {
+        statusCode: 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the note item.',
+      })
+    }
+  })
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(params.Item),
+  };
+
+  callback(null, response)
+    
+}
