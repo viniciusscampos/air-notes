4a8cf63f972cdaf7b0c399b0852d9968c00b912f
Vinicius Campos <silvcamposvinicius@gmail.com>
2018-06-22 01:40:28 -0300

    Fix update function.

--- back-end/air-notes-api/updateAnchor/handler.py
+++ back-end/air-notes-api/updateAnchor/handler.py
@@ -7,20 +7,26 @@ from decimal import Decimal
 def updateAnchor (event, context):
 
     try:
-        body = event["body"]
-        table = os.environ['DYNAMODB_TABLE']
+        body = json.loads(event["body"])
         dynamodb = boto3.resource('dynamodb')
-
-        dynamodb.update_item(TableName=table, 
-                                Key={'anchor': body["anchor"]}, 
-                                AttributeUpdates={"notes":body["notes"]}) 
+        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+        result = table.update_item(
+            Key={
+                'anchor': body["anchor"]
+            },
+            ExpressionAttributeValues={
+                ':notes': body['notes']
+            },
+            UpdateExpression='SET notes = :notes',
+             ReturnValues='ALL_NEW',
+        )
         return {
-            statusCode: 200,
-            body: json.dumps(body), ensure_ascii=False),
+            "statusCode": 200,
+            "body": json.dumps(result['Attributes'])
         }
 
-    except e:
+    except Exception as e:
         return {
-            statusCode: 500,
-            body: json.dumps({error:str(e)}, ensure_ascii=False),
+            "statusCode": 500,
+            "body": json.dumps({"error":str(e)}, ensure_ascii=False),
         }
