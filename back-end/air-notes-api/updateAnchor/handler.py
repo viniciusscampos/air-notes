3538a4073e79d2757af92813d59e8c465ecf0961
Vinicius Campos <silvcamposvinicius@gmail.com>
2018-06-27 00:48:22 -0300

    Convert to decimal.

--- back-end/air-notes-api/updateAnchor/handler.py
+++ back-end/air-notes-api/updateAnchor/handler.py
@@ -10,6 +10,17 @@ def updateAnchor (event, context):
         body = json.loads(event["body"])
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+        notes = body['notes']
+        for note in notes:
+            position = note['position']
+            rotation = note['rotation']
+            for axis in position:
+                value = str(position[axis])
+                position[axis] = Decimal(value)
+            for axis in rotation:
+                value = str(rotation[axis])
+                rotation[axis] = Decimal(value)
+        print("Body: {0}".format(body))
         result = table.update_item(
             Key={
                 'anchor': body["anchor"]
